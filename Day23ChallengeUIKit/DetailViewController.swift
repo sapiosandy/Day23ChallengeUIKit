72c3a3285935eed07683f3ca3783513e50995eee
Sandy Gomez <sgomez1029@gmail.com>
2024-02-28 21:49:39 -0800

    .dropLast inside for loop

--- Day23ChallengeUIKit/DetailViewController.swift
+++ Day23ChallengeUIKit/DetailViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    var selectedFlag: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = selectedFlag!.uppercased()
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedFlag {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Day23ChallengeUIKit/ViewController.swift
+++ Day23ChallengeUIKit/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var flags = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        title = "Countries"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        for item in items {
+            if item.contains("@2x") {
+                let newItem = String(item.dropLast(7))
+                flags.append(newItem)
+            }
+        }
+        flags.sort()
     }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row].uppercased()
+        cell.imageView?.image = UIImage(named: flags[indexPath.row])
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedFlag = flags[indexPath.row]
 
-
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
 
